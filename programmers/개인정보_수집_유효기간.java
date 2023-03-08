6b150e6e9735ef008784d23f4691bd666e17e2e3
abcodefg <a74566463@gmail.com>
2023-03-08 13:18:26 +0900

    개인정보 수집 유효기간(compareTo 활용한 String값들의 비교, 예외 케이스 확인(날짜 계산))

--- programmers/개인정보_수집_유효기간.java
+++ programmers/개인정보_수집_유효기간.java
@@ -20,19 +20,25 @@ import java.util.*;
  */
 public class 개인정보_수집_유효기간 {
     /**
-     * 간단한 구현 문제인데, 눈여겨 볼 점은 크게 두 가지이다.
+     * 간단한 구현 문제인데, 짚고 넘어갈 부분은 크게 두 가지이다.
      * 1. 날짜 계산시 예외 케이스에 유의한다.
+     *      이 풀이의 경우 수집일자에 유효기간을 더한 월이 12의 배수인 경우가 예외 케이스이며,
+     *      유효기간이 끝나는 연과 일을 따로 계산해주어야 한다.
+     *
+     *      예를 들어, 수집일자가 2022.06.01일 때, 유효기간이 3 / 13이라면
+     *      수집일자에 유효기간을 더한 값(month)인 9 / 19 를 12로 나눈 몫(month/12)을 수집일자의 연도에 더하고, 그 나머지가 월이 된다.
+     *
+     *      그런데 유효기간이 6이고, month가 12의 배수인 12라면
+     *      이를 12로 나눈 몫인 1을 연도에 더하고 나머지인 0이 월이 되면 2023.00.01이라는 잘못된 값이 나오므로 다른 연산을 적용해야 한다.
+     *      month - 1인 경우와 연도는 같고, 월은 1개월 차이 난다는 점을 활용해 변경한 연산을 적용한다.
+     *
      * 2. 약관별 유효기간을 확인하는 방법으로 privacies의 반복문 내부에 terms의 반복문을 순회하는 것 역시 가능하다.
      *      terms의 길이가 짧은 경우 이 방법의 수행 시간이 더 짧게 나오기도 하나,
      *      terms의 길이가 길어지고 매 privacies의 원소마다 비교해야 할 terms의 원소의 개수가 많아질수록
+     *      map에서 약관에 해당하는 유효기간을 바로 반환하게 하는 것이 map을 생성하는 과정을 감안하더라도
+     *      시간복잡도 측면에서 우수하다.
      */
-    public static void main(String[] args) {
-        String today = "2020.01.01";
-        String[] terms = {"Z 3", "D 5"};
-        String[] privacies = {"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"};
-        System.out.println(Arrays.toString(solution(today, terms, privacies)));
-    }
-    public static int[] solution(String today, String[] terms, String[] privacies) {
+    public int[] solution(String today, String[] terms, String[] privacies) {
 
         Map<String, String> map = new HashMap<>();
         for (String term : terms) {
@@ -47,6 +53,7 @@ public class 개인정보_수집_유효기간 {
             privacy = privacies[i].split(" ");
 
             String expirationDate = getExpirationDate(Integer.parseInt(map.get(privacy[1])), privacy[0]);
+            // TODO 확인할 것 (1) : compareTo()를 활용한 두 String 값의 비교
             if (today.compareTo(expirationDate) >= 0)
                 list.add(i + 1);
         }
@@ -59,13 +66,13 @@ public class 개인정보_수집_유효기간 {
         int month = Integer.parseInt(dateArray[1]);
         month += period;
 
-        // TODO 확인할 것 : 예외 케이스 확인할 것!
+        // TODO 확인할 것 (2) : 예외 케이스 확인할 것!
         if (month % 12 == 0) {
             dateArray[0] = String.valueOf(Integer.parseInt(dateArray[0]) + ((month-1) / 12));
-            dateArray[1] = String.valueOf(month - (12 * ((month-1) / 12)));
+            dateArray[1] = String.valueOf((month - 1) % 12 + 1);
         } else {
             dateArray[0] = String.valueOf(Integer.parseInt(dateArray[0]) + (month / 12));
-            dateArray[1] = String.valueOf(month - (12 * (month / 12)));
+            dateArray[1] = String.valueOf(month % 12);
         }
         dateArray[1] = dateArray[1].length() == 1 ? "0" + dateArray[1] : dateArray[1];
 
