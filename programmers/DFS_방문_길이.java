4fb992a209ac1f4a8d424edd9fc309eaee8878a0
abcodefg <a74566463@gmail.com>
2023-03-15 00:35:16 +0900

    방문 길이(set을 활용한 중복 count 회피)

--- programmers/DFS_방문_길이.java
+++ programmers/DFS_방문_길이.java
@@ -14,8 +14,8 @@ public class DFS_방문_길이 {
      * 다른 dfs와 굳이 다른 게 있다면 visited 배열을 좌표 + 방향에 대해 나타내기 위해서 3차원으로 생성했다는 점이다.
      *
      * 통과는 하긴 했지만 너무 기계적인 접근인 데다가, 부수적으로 선언해야 하는 변수들이 많았고
-     * 특히 visited 배열은 인접해 있는 다른 두 좌표의 반대 방향으로의 이동은 같은 길을 지나가므로
-     * visited 값을 개별적으로 표현할 필요가 없다는 점에서 불필요하게 컸다.
+     * 특히 인접해 있는 다른 두 좌표의 반대 방향으로의 이동은 같은 길을 지나가므로
+     * visited 값을 개별적으로 표현할 필요가 없다는 점에서 visited 배열이 불필요하게 컸다.
      *
      * Set을 활용한 멋진 풀이가 있어서 같이 기록한다.
      * 같은 길을 지나가는 이동을 자료구조와 간단한 연산으로 간결하게 세는 멋진 해법이다.
@@ -68,14 +68,18 @@ public class DFS_방문_길이 {
         }
     }
 
-    public int solution2(String dirs) {
+    public static void main(String[] args) {
+        System.out.println(solution2("ULURRDLLU"));
+    }
+
+    public static int solution2(String dirs) {
         Map<Character, int[]> map = new HashMap<>();
         map.put('U', new int[]{0, -1});
         map.put('D', new int[]{0, 1});
         map.put('L', new int[]{-1, 0});
         map.put('R', new int[]{1, 0});
 
-        Set<int[]> set =  new HashSet<>();
+        Set<String> set =  new HashSet<>();
         int x = 0, y = 0, nx, ny;
         char[] dirArr = dirs.toCharArray();
 
@@ -84,8 +88,10 @@ public class DFS_방문_길이 {
             ny = y + map.get(dirArr[i])[1];
 
             if (nx >= -5 && ny >= -5 && nx <= 5 && ny <= 5) {
-                set.add(new int[]{x, y, nx, ny});
-                set.add(new int[]{nx, ny, x, y});
+                set.add(x + "" + y + "" + nx + "" + ny);
+                set.add(nx + "" + ny + "" + x + "" + y);
+                x = nx;
+                y = ny;
             }
         }
 
