4b374472f36b41dc33f24b361519bca2ceb7f2fa
abcodefg <a74566463@gmail.com>
2023-05-16 23:50:08 +0900

    Stack을 활용한 백트래킹

--- programmers/여행_경로/Solution.java
+++ programmers/여행_경로/Solution.java
@@ -5,6 +5,12 @@ import java.util.*;
 public class Solution {
     /**
      * [정렬한 tickets 배열과 재귀를 활용한 풀이]
+     * 출발지가 동일한 항공권들끼리 알파벳 순으로 모이게끔 tickets를 정렬한다.
+     *
+     * 'ICN'에서 시작해 경로 상의 도시에서 사용할 수 있는 항공권을 순서대로 조회해서 도착지를 path에 추가하고
+     * 모든 항공권을 사용하면 재귀 DFS를 종료한다.
+     *
+     * 매 재귀 호출시에 for 문에서 모든 항공권의 개수(=used.length)만큼 순회해야 하기 때문에 효율적인 풀이는 아니다.
      */
     private boolean found;
     private String ans;
@@ -12,7 +18,7 @@ public class Solution {
         // 재귀 호출을 빠르게 종료하기 위한 boolean 변수
         found = false;
 
-        // tickets 배열을 알파벳 순으로 정렬한다
+        // tickets 배열을 출발지 알파벳 순 -> 도착지 알파벳 순으로 정렬한다
         Arrays.sort(tickets, (o1, o2)
                 -> o1[0].equals(o2[0]) ? o1[1].compareTo(o2[1]) : o1[0].compareTo(o2[0]));
 
@@ -46,8 +52,10 @@ public class Solution {
      * [인접 리스트와 스택을 활용한 풀이]
      */
     public String[] solution_stack(String[][] tickets) {
+        // 여행 경로를 담는 리스트(역순으로 채워넣는다)
         List<String> path = new ArrayList<>();
 
+        // 출발지 - 도착지의 인접 리스트를 생성한다
         Map<String, List<String>> graph = new HashMap<>();
         for (String[] ticket : tickets) {
             String start = ticket[0];
@@ -56,6 +64,7 @@ public class Solution {
             graph.get(start).add(end);
         }
 
+        // 출발지별 도착지의 리스트를 알파벳 역순으로 정렬한다
         for (List<String> destinations : graph.values()) {
             Collections.sort(destinations, Collections.reverseOrder());
         }
@@ -64,13 +73,14 @@ public class Solution {
         stack.push("ICN");
 
         while (!stack.isEmpty()) {
-            String top = stack.pop();
-
-            if (!graph.containsKey(top) || graph.get(top).isEmpty()) {
-                path.add(top);
+            String cur = stack.pop();
+            // 현재 도시를 출발지로 하는 항공권이 없거나 다 사용한 경우 경로에 추가한다
+            // 해당 도시는 dead end이므로, 그 도시가 Stack에 담기게 해준 항공권을 다른 모든 항공권의 사용이 이루어진 후에 사용한다
+            if (!graph.containsKey(cur) || graph.get(cur).isEmpty()) {
+                path.add(cur);
             } else {
-                stack.push(top);
-                stack.push(graph.get(top).remove(graph.get(top).size() - 1));
+                stack.push(cur);
+                stack.push(graph.get(cur).remove(graph.get(cur).size() - 1));
             }
         }
 
